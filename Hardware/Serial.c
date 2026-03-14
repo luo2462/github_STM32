8ee1d65aa9165fb24cd0221875f2f641e4f99787
luo2462 <3595202955@qq.com>
2026-03-14 15:04:16 +0800

    2026.3.14    15：03

--- Hardware/Serial.c
+++ Hardware/Serial.c
@@ -1,4 +1,10 @@
 #include "stm32f10x.h"                  // Device header
+#include <stdio.h>
+#include <stdarg.h>
+
+
+uint8_t Serial_RxData;
+uint8_t Serial_RxFlag;
 
 void Serial_Init(void)
 {
@@ -11,12 +17,120 @@ void Serial_Init(void)
 	GPIO_InitStructure.GPIO_Speed=GPIO_Speed_50MHz;
 	GPIO_Init(GPIOA,&GPIO_InitStructure);
 	
+	GPIO_InitStructure.GPIO_Mode=GPIO_Mode_IPU;
+	GPIO_InitStructure.GPIO_Pin=GPIO_Pin_10;
+	GPIO_InitStructure.GPIO_Speed=GPIO_Speed_50MHz;
+	GPIO_Init(GPIOA,&GPIO_InitStructure);
+	
 	USART_InitTypeDef USART_InitStructure;
-	USART_InitStructure.USART_BaudRate=9600;  //波特率设置
-	USART_InitStructure.USART_HardwareFlowControl=USART_HardwareFlowControl_None;  //硬件流控制
-	USART_InitStructure.USART_Mode=USART_Mode_Tx;  //串口模式，TX发送模式，RX接受模式，如果两种都有，可以用 | (或符号)像pin口一样
-	USART_InitStructure.USART_Parity=USART_Parity_No;  //校验位，Odd奇校验，even偶校验，no无校验
-	USART_InitStructure.USART_StopBits=USART_StopBits_1;  //停止位，设定停止位数目，一般为1
-	USART_InitStructure.USART_WordLength=USART_WordLength_8b;  //字长，即数据位，通常，有校验位9，无校验位8
+	USART_InitStructure.USART_BaudRate=9600;                                        //波特率设置
+	USART_InitStructure.USART_HardwareFlowControl=USART_HardwareFlowControl_None;   //硬件流控制
+	USART_InitStructure.USART_Mode=USART_Mode_Tx | USART_Mode_Rx;                   //串口模式，TX发送模式，RX接受模式，如果两种都有，可以用 | (或符号)像pin口一样
+	USART_InitStructure.USART_Parity=USART_Parity_No;                               //校验位，Odd奇校验，even偶校验，no无校验
+	USART_InitStructure.USART_StopBits=USART_StopBits_1;                            //停止位，设定停止位数目，一般为1
+	USART_InitStructure.USART_WordLength=USART_WordLength_8b;                       //字长，即数据位，通常，有校验位9，无校验位8
 	USART_Init(USART1,&USART_InitStructure);
+	
+	USART_ITConfig(USART1,USART_IT_RXNE,ENABLE);
+	
+	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);  //依旧优先级配置
+	
+	NVIC_InitTypeDef NVIC_InitStructure;  //NVIC配置
+	NVIC_InitStructure.NVIC_IRQChannel=USART1_IRQn;
+	NVIC_InitStructure.NVIC_IRQChannelCmd=ENABLE;
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority=1;
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority=1;
+	NVIC_Init(&NVIC_InitStructure);
+	
+	USART_Cmd(USART1,ENABLE);
+}
+
+void Serial_SendByte(uint8_t Byte)
+{
+	USART_SendData(USART1,Byte);
+	while(USART_GetFlagStatus(USART1,USART_FLAG_TXE)==RESET);
+}//发送字节
+
+void Serial_SendArray(uint8_t *Array,uint16_t Length)
+{
+	uint16_t i;
+	for(i=0;i<Length;i++)
+	{
+		Serial_SendByte(Array[i]);
+	}
+}//发送一个数组
+
+void Serial_SendString(char *String)
+{
+		uint16_t i;
+		for(i=0;String[i] != '\0';i++)    //String[i]!='\0'   和  String[i]!=0  效果一样
+		{
+			Serial_SendByte(String[i]);
+		}
+}//遍历发送字符串
+
+uint32_t Serial_Pow(uint32_t X,uint32_t Y)
+{
+	uint32_t Result=1;
+	while(Y --)
+	{
+		Result *=X;
+	}
+	return Result;
+}//通过循环累乘实现幂运算
+
+void Serial_SendNumber(uint32_t Number,uint8_t Length)
+{
+	uint8_t i;
+	for(i=0;i<Length;i++)
+	{
+		Serial_SendByte(Number / Serial_Pow(10,Length-1-i)%10+'0');
+	}
+}//发送数字
+
+int fputc(int ch,FILE *f)
+{
+	Serial_SendByte(ch);
+	return ch;
+}//用printf函数重定向
+//如果多个串口都想用printf函数时
+//可以在main.c中用sprintf
+//以下为示例
+//char string[n];   n为长度，给够就行，100
+//sprintf(string,"要发送的数据");
+//Serial_SendString(string);
+
+void Serial_Printf(char *format,...)
+{
+	char string[100];
+	va_list arg;//定义一个参数列表变量
+	va_start(arg,format);//从format位置开始接受参数表，放在arg中
+	vsprintf(string,format,arg);//打印位置是string,格式化字符串是format,参数表是arg
+	va_end(arg);//释放arg
+	Serial_SendString(string);
+}//发送中文有两种方式，编码UTF-8和GB2312，保存关闭再打开
+
+uint8_t Serial_GetRxFlag(void)
+{
+	if(Serial_RxFlag==1)
+	{
+		Serial_RxFlag=0;
+		return 1;
+	}
+	return 0;
+}
+
+uint8_t Serial_GetRxData(void)
+{
+	return Serial_RxData;
+}
+
+void USART1_IRQHandler(void)
+{
+	if(USART_GetITStatus(USART1,USART_IT_RXNE)==SET)
+	{
+		Serial_RxData=USART_ReceiveData(USART1);
+		Serial_RxFlag=1;
+		USART_ClearITPendingBit(USART1,USART_IT_RXNE);
+	}
 }
